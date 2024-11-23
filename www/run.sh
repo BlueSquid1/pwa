23df709c275fb1dced1485e35f767c950e128854
clinton <clinton.page123@gmail.com>
2024-11-23 14:12:20 +1030

    updated www to generate a tar archive

--- www/run.sh
+++ www/run.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 bazel build :generate_html
-python3 -m webbrowser "file://${SCRIPT_DIR}/bazel-bin/dist/index.html"
+output_files=(`bazel cquery :generate_html --output files 2>/dev/null`)
+index_html=""
+for file in "${output_files[@]}"; do
+  if [[ $file == *"index.html"* ]]; then
+    index_html=$file
+    break
+  fi
+done
+python3 -m webbrowser "file://${SCRIPT_DIR}/${index_html}"
